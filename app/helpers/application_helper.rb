8184d887d60e14d3c73b8138fd0edb9f96d68ff9
MohamedBouzidi <mohamedbouzidi95@hotmail.com>
2017-02-22 10:18:50 +0100

    Finish static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Returns the appropriete page title.
+  def full_title(page_title)
+    @base_title = "Rails Social Network"
+    if !page_title.blank?
+      "#{page_title} | #{@base_title}"
+    else
+      "#{@base_title}"
+    end
+  end
+
 end
